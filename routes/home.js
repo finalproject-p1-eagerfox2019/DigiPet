392d780d326e04216e35e418c42cd17ea5c32279
emji-MG <mikelgunawan95@gmail.com>
2019-05-22 11:42:40 +0700

    home page

---
+++ routes/home.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+const Model = require('../models')
+
+
+router.get('/', (req, res) => {
+    res.render('home.ejs', {
+        sendDisplayError : req.query.displayError
+    })
+})
+// router.post('/home', (req, res) => {
+//     User.findAll({
+//         where : {
+//             name : req.body.name,
+//             email : req.body.email,
+//             password : req.body.password
+//         }
+//     })
+//     .then(dataOne => {
+//         // console.log(dataOne, '========================')
+//         if(dataOne.length === 0){
+//             let objCreate = {
+//                 name : req.body.name,
+//                 balance : 100,
+//                 email : req.body.email,
+//                 password : req.body.password,
+//                 petQuantity : 0
+//             }
+//             return User.create(objCreate)
+//         }else{
+//             let errMsg = 'emali already taken'
+//             res.redirect(`/register?displayError=${errMsg}`)
+//         }
+//     })
+//     // User.create(objCreate)
+//     .then((data) => {
+//         // res.send(data)
+//         res.redirect('/register')
+//     })
+//     .catch(err => {
+//         res.send(err)
+//     })
+// })
+
+module.exports = router
